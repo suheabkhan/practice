9ae4f1b9fc17ef93857945d0f60ffc06de662378
suheab khan <khansuheab@gmail.com>
2022-06-04 13:29:35 +0530

    Added a new line

--- java/Test.java
+++ java/Test.java
@@ -7,5 +7,6 @@ public class Test{
         System.out.println("Enter a number");
         int number=s.nextInt();
         System.out.println("You have entered "+number);
+        System.out.println("New Element added");
     }
 }
