f96fd2b62f8e6dc40442322d15a453747304defc
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2022-06-04 13:27:07 +0530

    Add files via upload

---
+++ java/Test.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number=s.nextInt();
+        System.out.println("You have entered "+number);
+    }
+}
